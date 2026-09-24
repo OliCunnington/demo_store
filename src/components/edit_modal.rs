1ed552b40fb0d02c5b0b0bbcac97a1cfe921a43f
Oliver Cunnington <ofcunnington@hotmail.com>
2026-09-24 08:18:35 +0200

    reusable components

--- src/components/edit_modal.rs
+++ src/components/edit_modal.rs
@@ -5,6 +5,12 @@ pub fn EditModal(
     dialog_ref: NodeRef<html::Dialog>
 ) -> impl IntoView {
     view!{
-        <p>"Placeholder"</p>
+        <dialog node_ref=dialog_ref class="modal">
+            <p>"Placeholder"</p>
+        </dialog>
     }
 }
+
+
+// hmm, considering this needs to be context dependent... it should probably
+// take either a view or specifically a form as an arg? 
